2fb268439bb38f42d01505834ce71ae789ffc2de
maddenlockin <maddenlockin@gmail.com>
2021-11-05 15:19:28 -0700

    container built out

--- src/components/name-tag/Controls.jsx
+++ src/components/name-tag/Controls.jsx
@@ -1,49 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Controls = ({
-    greeting,
-    name,
-    pronouns,
-    onGreetingChange,
-    onNameChange,
-    onPronounChange,
-    onSubmit
+  greeting,
+  name,
+  pronouns,
+  onGreetingChange,
+  onNameChange,
+  onPronounChange,
+  onSubmit,
 }) => {
-    return (
-        <form onSubmit={onSubmit}>
-            <label htmlFor='greeting'>Greeting</label>
-            <input
-                id='greeting'
-                name='greeting'
-                type='text'
-                value={greeting}
-                onChange={onGreetingChange}
-            />
-            <label htmlFor='name'>Name</label>
-            <input
-                id='name'
-                name='name'
-                type='text'
-                value={name}
-                onChange={onNameChange}
-            />
-            <label htmlFor='pronouns'>Pronouns</label>
-            <input
-                id='pronouns'
-                name='pronouns'
-                type='text'
-                value={pronouns}
-                onChange={onPronounChange}
-            />
-            <button aria-label='submit'>Submit</button>
-        </form>
-    );
-}
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="greeting">Greeting</label>
+      <input
+        id="greeting"
+        name="greeting"
+        type="text"
+        value={greeting}
+        onChange={onGreetingChange}
+      />
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        value={name}
+        onChange={onNameChange}
+      />
+      <label htmlFor="pronouns">Pronouns</label>
+      <input
+        id="pronouns"
+        name="pronouns"
+        type="text"
+        value={pronouns}
+        onChange={onPronounChange}
+      />
+      <button aria-label="submit">Submit</button>
+    </form>
+  );
+};
 
 Controls.propTypes = {
-    greeting: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    pronouns: PropTypes.string.isRequired,
+  greeting: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  pronouns: PropTypes.string.isRequired,
+  onGreetingChange: PropTypes.func.isRequired,
+  onNameChange: PropTypes.func.isRequired,
+  onPronounChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
 export default Controls;
--- src/containers/NameTag.jsx
+++ src/containers/NameTag.jsx
@@ -1,5 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Controls from "../components/Controls";
+import Display from "../components/Display";
 
 export default class NameTag extends Component {
+  state = {
+    greeting: "",
+    name: "",
+    pronouns: "",
+    submitGreet: "",
+    submitName: "",
+    submitPros: "",
+  };
 
+  handleGreetingChange = (e) => {
+    this.setState({ greeting: e.target.value });
+  };
+
+  handleNameChange = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  handlePronounChange = (e) => {
+    this.setState({ pronouns: e.target.value });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    this.setState({
+      submitGreet: this.state.greeting,
+      submitName: this.state.name,
+      submitPros: this.state.pronouns,
+    });
+  };
+
+  render() {
+    const { name, pronouns, greeting, submitName, submitGreet, submitPros } =
+      this.state;
+    return (
+      <>
+        <Controls
+          greeting={greeting}
+          onGreetingChange={this.handleGreetingChange}
+          name={name}
+          onNameChange={this.handleNameChange}
+          pronouns={pronouns}
+          onPronounChange={this.handlePronounChange}
+          onSubmit={this.handleSubmit}
+        />
+        <Display
+          submitGreet={submitGreet}
+          submitName={submitName}
+          submitPros={submitPros}
+        />
+      </>
+    );
+  }
 }
