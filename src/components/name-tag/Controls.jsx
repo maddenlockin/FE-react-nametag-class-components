c29e0d0944597901059e550edfd5bd512d04ea24
maddenlockin <maddenlockin@gmail.com>
2021-11-05 15:08:05 -0700

    controls built out

--- src/components/name-tag/Controls.jsx
+++ src/components/name-tag/Controls.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Controls = () => ();
+const Controls = ({
+    greeting,
+    name,
+    pronouns,
+    onGreetingChange,
+    onNameChange,
+    onPronounChange,
+    onSubmit
+}) => {
+    return (
+        <form onSubmit={onSubmit}>
+            <label htmlFor='greeting'>Greeting</label>
+            <input
+                id='greeting'
+                name='greeting'
+                type='text'
+                value={greeting}
+                onChange={onGreetingChange}
+            />
+            <label htmlFor='name'>Name</label>
+            <input
+                id='name'
+                name='name'
+                type='text'
+                value={name}
+                onChange={onNameChange}
+            />
+            <label htmlFor='pronouns'>Pronouns</label>
+            <input
+                id='pronouns'
+                name='pronouns'
+                type='text'
+                value={pronouns}
+                onChange={onPronounChange}
+            />
+            <button aria-label='submit'>Submit</button>
+        </form>
+    );
+}
 
-Controls.propTypes = {};
-
+Controls.propTypes = {
+    greeting: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    pronouns: PropTypes.string.isRequired,
+};
 export default Controls;
